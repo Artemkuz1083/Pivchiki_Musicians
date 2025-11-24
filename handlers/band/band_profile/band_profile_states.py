9e8979f7c13b6d39a4dd74c7eed48710a9e4a933
DaniilPrikhoddko <d1912dd@gmail.com>
2025-11-24 12:01:39 +0500

    registration for band

--- handlers/band/band_profile/band_profile_states.py
+++ handlers/band/band_profile/band_profile_states.py
@@ -5,3 +5,7 @@ class BandEditingStates(StatesGroup):
     editing_band_year = State()
     editing_genres = State()
     inputting_own_genre = State()
+    editing_city = State()
+    inputting_own_city = State()
+    editing_description = State()
+    editing_seriousness_level = State()
